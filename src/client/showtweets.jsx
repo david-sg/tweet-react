52c65e72f31985fe49c1cbef1c1f3a9e6396e93c
David M <davidmosk@gmail.com>
2019-11-20 18:08:21 +0800

    test show tweets

--- src/client/showtweets.jsx
+++ src/client/showtweets.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-
-
-
-
   class Showtweets extends React.Component {
 
     render() {
   
       const tweety = this.props.tweets.map( tweet => {
-        return 
-        <li>{tweet.user.screen_name}</li>
-        
-
+        console.log(tweet.user.screen_name)
+        return <li>{tweet.text}</li>
       });
       return (
         <div>
